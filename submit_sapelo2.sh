4a76d0248daad6d40b1702ee2cea6827add5e3db
rx32940 <rx32940@uga.edu>
2021-02-14 15:09:30 -0500

    run fastbap lineage 5

--- submit_sapelo2.sh
+++ submit_sapelo2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --partition=batch
-#SBATCH --job-name=snakemake_intLineage4
+#SBATCH --job-name=snakemake_intLineage5
 #SBATCH --ntasks=1                    	
 #SBATCH --cpus-per-task=10             
 #SBATCH --time=150:00:00
 #SBATCH --mem=100G
-#SBATCH --output=/scratch/rx32940/snakemake_intLineage4.%j.out       
-#SBATCH --error=/scratch/rx32940/snakemake_intLineage4.%j.out        
+#SBATCH --output=/scratch/rx32940/snakemake_int_lineage_5_fastbaps.%j.out       
+#SBATCH --error=/scratch/rx32940/snakemake_int_lineage_5_fastbaps.%j.out        
 #SBATCH --mail-user=rx32940@uga.edu
 #SBATCH --mail-type=ALL
 
@@ -16,6 +16,6 @@ cd /home/rx32940/github/Bacterial_Genome_Analysis_Toolbox/
 source activate /home/rx32940/miniconda3/envs/Bacterial_Genome_Analysis_Toolbox 
 
 snakemake --cores 10 --use-conda
-snakemake --rulegraph| dot -Tpdf > dag.svg
+snakemake --rulegraph| dot -Tpdf > dag.pdf
 
 conda deactivate
