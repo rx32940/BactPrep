0b977ae2c0a6c31b7ebcb425810841c387aec325
rx32940 <rx32940@uga.edu>
2021-02-20 16:53:54 -0500

    readme

--- submit_sapelo2.sh
+++ submit_sapelo2.sh
@@ -5,8 +5,8 @@
 #SBATCH --cpus-per-task=10             
 #SBATCH --time=150:00:00
 #SBATCH --mem=100G
-#SBATCH --output=/scratch/rx32940/snakemake_asiaIntRef.%j.out       
-#SBATCH --error=/scratch/rx32940/snakemake_asiaIntRef.%j.out        
+#SBATCH --output=/scratch/rx32940/snakemake_asiaIntSubset.%j.out       
+#SBATCH --error=/scratch/rx32940/snakemake_asiaIntSubset.%j.out        
 #SBATCH --mail-user=rx32940@uga.edu
 #SBATCH --mail-type=ALL
 
