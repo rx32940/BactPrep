3f2f9d9f4f0df800caa4a464d597349ecd0856d9
rx32940 <rx32940@uga.edu>
2021-03-18 18:25:48 -0400

    commit

--- submit_sapelo2.sh
+++ submit_sapelo2.sh
@@ -15,7 +15,7 @@
 cd $SLURM_SUBMIT_DIR
 
 ml snakemake/5.7.1-foss-2019b-Python-3.7.4
->>>>>>> 79c4a25a2ac90d80e107b90c0acb4bc4e9013152
+
 
 snakemake --cores 10 --use-conda
 snakemake --rulegraph| dot -Tpdf > dag.pdf
