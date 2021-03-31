e90676d575df80735f0f32f28cc9610eeacb87a4
rx32940 <rx32940@uga.edu>
2021-03-31 12:59:46 -0400

    commit

--- workflow/scripts/change_fasta_header.py
+++ workflow/scripts/change_fasta_header.py
@@ -14,19 +14,20 @@ for item in items_list:
     temp_dict=metadata.loc[:,[dict_key,item]].set_index(dict_key).to_dict()[item]
     dict_list.append(temp_dict)
 
-with open(str(sys.argv[5]), "w") as w:
+with open(str(sys.argv[5]), "w+") as w:
     print("start a new file, overwrriten the old file if exist")
 
 with open(str(sys.argv[2])) as f, open(str(sys.argv[5]), "w") as w:
     records = SeqIO.parse(f,"fasta")
     for r in records:
         old_id = r.id
-        new_name=""
+        new_name=r.id
         for d in range(len(dict_list)):
             current_dict= dict_list[d]
             current_meta=current_dict[old_id]
             new_name = new_name + "|" + str(current_meta) 
         r.id = new_name
         r.description = ""
+        print(r.id)
         SeqIO.write(r, w, "fasta")
 
