9339dc3442bd804d77c2d5e95b1484a56b780f37
rx32940 <rx32940@uga.edu>
2021-07-29 11:32:54 -0400

    commit

--- submit_sapelo2.sh
+++ submit_sapelo2.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=sub.sh
 #SBATCH --ntasks=1                    	
 #SBATCH --cpus-per-task=5      
-#SBATCH --time=150:00:00
+#SBATCH --time=100:00:00
 #SBATCH --mem=100G
 #SBATCH --output=../%x.%j.out       
 #SBATCH --error=../%x.%j.out        
@@ -22,13 +22,15 @@ cd $SLURM_SUBMIT_DIR
 
 WORKPATH="/scratch/rx32940/PMEN1"
 
-time python start_analysis.py ALL \
+time python start_analysis.py coreRecomb \
 -p PMEN1.dated \
 -o $WORKPATH -i $WORKPATH/assemblies \
--r $WORKPATH/GCF_000026665.1_ASM2666v1_genomic.fna -G " -f 30" -t 5 \
 -M \
 -a $WORKPATH/PMEN1.dated.metadata.csv \
--m Year,Country
+-m Year,Country \
+-r $WORKPATH/GCF_000026665.1_ASM2666v1_genomic.fna \
+-G " -f 30" -t 5 
+
 
 
 conda deactivate
